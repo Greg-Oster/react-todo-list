74074b063295644499fa95a29a89b27512ffe639
Grisha Ostroukhov <ranni91@gmail.com>
2020-08-27 03:23:44 +0300

    all setups is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import uuid from "uuid";
+import { v1 as uuid } from "uuid";
 
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
@@ -7,7 +7,32 @@ import TodoInput from "./components/TodoInput";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
+  state = {
+    id: uuid(),
+    items: [
+      { id: 1, title: "wake up" },
+      { id: 2, title: "have sex" },
+    ],
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle submit");
+  };
+  clearList = () => {};
+  handleDelete = (id) => {
+    console.log(`delete ${id}`);
+  };
+  handleEdit = (id) => {
+    console.log(`edit ${id}`);
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <div className="container">
